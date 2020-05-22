d8ad371a62936a890987127ced62495652943c28
Glory David <glodave99@gmail.com>
2020-05-22 09:58:23 +0100

    Added selection

--- bin/main.rb
+++ bin/main.rb
@@ -3,5 +3,7 @@
 puts "Welcome to Tic-Tac_Toe!\n\n"
 puts "Please enter your name, Player1?\n"
 player_1 = gets.chomp
+puts "Please select X or O\n"
+select_1 = gets.chomp
 puts "Please enter your name, Player2?\n"
 player_2 = gets.chomp

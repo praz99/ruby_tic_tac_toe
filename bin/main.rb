08db005434f63678b3b3a5c8bab7b079ed6ce129
Glory David <glodave99@gmail.com>
2020-05-22 10:24:37 +0100

    Added conditions for players

--- bin/main.rb
+++ bin/main.rb
@@ -5,5 +5,15 @@ puts "Please enter your name, Player1?\n"
 player_1 = gets.chomp
 puts "Please select X or O\n"
 select_1 = gets.chomp
+if select_1 == "X"
+  puts "#{player_1} has selected X"
+  select_2 = "O"
+elsif select_1 == "O"
+  puts "O"
+  select_2 = "X"
+else 
+  puts "Please enter X or O"
+end
 puts "Please enter your name, Player2?\n"
 player_2 = gets.chomp
+puts "#{player_2} is #{select_2}"

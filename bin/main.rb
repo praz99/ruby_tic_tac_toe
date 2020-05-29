437cc78ea57f814543c6c6dca96a95a08f465bbe
Glory David <glodave99@gmail.com>
2020-05-29 13:43:02 +0100

    added game logic

--- bin/main.rb
+++ bin/main.rb
@@ -1,92 +1,119 @@
 #!/usr/bin/env ruby
-require_relative '../lib/name_check.rb'
-require_relative '../lib/board.rb'
 
-# Welcome Message and player name input.
+require_relative '../lib/game.rb'
 puts ".....Welcome to Tic-Tac_Toe!.....\n\n"
-puts 'Please enter your name Player_one:'
-player_one = gets.chomp.capitalize
-puts "\n"
+puts "  ---+---+---+--"
+puts "  #  |  #  |  # "
+puts "  ---+---+---+--"
+puts "  #  |  #  |  # "
+puts "  ---+---+---+--"
+puts "  #  |  #  |  # "
+puts "  ---+---+---+--"
 
-valid_name = NameCheck.new
-until valid_name.name_given?(player_one)
-  puts 'Name cannot be empty. Please input a name.'
+
+puts "This is game of 'X' and 'O'\n"
+
+play = true
+
+while play
+  game = Game.new([' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '])
+  puts 'Please enter your name Player One:'
   player_one = gets.chomp.capitalize
-end
 
-puts 'Please enter your name Player_two:'
-player_two = gets.chomp.capitalize
-puts "\n"
-check = true
-while check
-  if !valid_name.name_given?(player_two)
-    puts 'Name cannot be empty. Please input a name.'
-    player_two = gets.chomp.capitalize
-  elsif valid_name.name_repeated?(player_one, player_two)
-    puts 'The name has been repeated, please select a different name player_two.'
-    player_two = gets.chomp.capitalize
-  else
-    check = false
+  while player_one.empty?
+    puts "Please enter your name!"
+    player_one = gets.chomp.capitalize
   end
-end
 
-# Player input symbol selection.
-select_one = ''
-until select_one.upcase == 'X' || select_one == 'O'
-  puts "#{player_one}, please select 'X' or 'O'"
+  puts "Select either 'X' or 'O'"
   select_one = gets.chomp.upcase
-end
 
-select_two = select_one == 'X' ? 'O' : 'X'
-
-puts "#{player_one} has selected #{select_one}."
-puts "Therefore #{player_two} will use #{select_two}."
-puts ''
-puts 'The board is displayed'
-
-board = Board.new
-puts board.display_board
-
-def find_winner
-  puts 'check win condition.'
-  puts 'check draw condition'
-  puts 'If win condition or draw, end game else continue game.'
-end
-
-def valid_move?(move)
-  (1..9).include?(move)
-end
-
-current_player = player_one
-game = true
-
-while game
-  puts ''
-  if current_player == player_one
-    puts "#{player_one}, please enter your move."
-    input = gets.chomp.to_i # Current player's input.
-    until valid_move?(input)
-      puts 'Invalid move, please choose another move.'
-      input = gets.chomp.to_i
-    end
-    if board.update_board(input, select_one)
-      puts board.display_board
-      current_player = player_two
-    else
-      puts "Error"
-    end
-  else
-    puts "#{player_two}, please enter your move."
-    input = gets.chomp.to_i # Current player's input.
-    until valid_move?(input)
-      puts 'Invalid move, please choose another move.'
-      input = gets.chomp.to_i
-    end
-    board.update_board(input, select_two)
-    puts board.display_board
-    current_player = player_one
+  until %w[X O].include?(select_one)
+    puts "Please select 'X' or 'O'"
+    letter = gets.chomp.upcase
+    select_one = letter if Game.valid_letter?(letter)
   end
-  # game = false if find_winner
-end
 
-puts '.....GAME OVER.....'
+  game.player1_name = player_one
+  game.player1_select = select_one
+
+  puts "#{player_one} chose #{select_one}"
+
+  puts 'Please enter your name Player Two:'
+  player_two = gets.chomp.capitalize
+
+  while player_two.empty?
+    puts "Please enter your name!"
+    player_two = gets.chomp.capitalize
+  end
+
+  select_two = select_one == 'X' ? 'O' : 'X'
+  puts "Therefore #{player_two} will use #{select_two}."
+
+  game.player2_name = player_two
+  game.player2_select = select_two
+
+
+  game_is_on = true
+
+  puts "  ---+---+---+--"
+  puts "  1  |  2  |  3 "
+  puts "  ---+---+---+--"
+  puts "  4  |  5  |  6 "
+  puts "  ---+---+---+--"
+  puts "  7  |  8  |  9 "
+  puts "  ---+---+---+--"
+
+  i = 0
+
+  while game_is_on
+    game.current_player(i)
+    puts "#{game.name_current_player} please make a move from 1 to 9 "
+    move = gets.chomp
+    until game.valid_move?(move)
+      puts "Error, input invalid. #{game.name_current_player} Please try again"
+      move = gets.chomp
+    end
+
+    board = game.add_to_board(move.to_i)
+
+    puts "  ---+---+---+--"
+    puts "  #{board[0]}  |  #{board[1]}  |  #{board[2]} "
+    puts "  ---+---+---+--"
+    puts "  #{board[3]}  |  #{board[4]}  |  #{board[5]} "
+    puts "  ---+---+---+--"
+    puts "  #{board[6]}  |  #{board[7]}  |  #{board[8]} "
+    puts "  ---+---+---+--"
+
+    win = game.win?
+
+    if win
+      puts "#{game.name_current_player} has won the game!"
+      game_is_on = false
+    end
+
+    draw = game.no_win?
+    if draw && !win
+      puts "It's a Tie!!!"
+      game_is_on = false
+    end
+    i += 1
+  end
+
+  puts "Would you like to start again?"
+  puts "Type 'y' for Yes or 'n' for No"
+
+  while choice = gets.chomp.downcase 
+    if choice == 'y'
+      continue = true
+      break
+    elsif choice == 'n'
+      continue = false
+      break
+    else
+      puts "Please, enter 'y' for Yes or 'n' for No"
+      choice
+    end
+    choice
+  end
+end
--- lib/board.rb
+++ lib/board.rb
@@ -1,33 +1,25 @@
+#update board
 class Board
-  def initialize
-    @board = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+  def self.win?(arr, player)
+    return true if [
+      arr[0..2].all?(player),
+      arr[3..5].all?(player),
+      arr[6..8].all?(player),
+
+      [arr[0], arr[3], arr[6]].all?(player),
+      [arr[1], arr[4], arr[7]].all?(player),
+      [arr[2], arr[5], arr[8]].all?(player),
+
+      [arr[0], arr[4], arr[8]].all?(player),
+      [arr[6], arr[4], arr[2]].all?(player)
+    ].any?
+
+    false
   end
 
-  def display_board
-    "           |     |
-        #{@board[0][0]}  |  #{@board[0][1]}  |  #{@board[0][2]}
-      _____|_____|_____
-           |     |
-        #{@board[1][0]}  |  #{@board[1][1]}  |  #{@board[1][2]}
-      _____|_____|_____
-           |     |
-        #{@board[2][0]}  |  #{@board[2][1]}  |  #{@board[2][2]}
-           |     |"
-  end
-
-  def update_board(num, sym)
-    i = 0
-    while i < 3
-      j = 0
-      while j < 3
-        if @board[i][j] == num
-          @board[i][j] = sym
-          return true
-        end
-        j += 1
-      end
-      i += 1
-    end
-    return false
+  def self.draw?(arr)
+    return false if arr.include?(' ')
+    true
   end
 end
+#win conditions
